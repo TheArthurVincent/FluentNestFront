d8e60f2e0edc7bf0033f51c4b7cdcfc65a9e3a36
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-06-26 07:30:10 -0300

    Refactor Blog component by removing unused state, functions, and modal implementation for cleaner code.

--- src/Routes/HomePage/HomePageContent.tsx
+++ src/Routes/HomePage/HomePageContent.tsx
@@ -1,25 +1,16 @@
 import React, { useEffect, useState } from "react";
-import {
-  RouteDiv,
-  BackgroundClickBlog,
-  HTwo,
-  HOne,
-} from "../../Resources/Components/RouteBox";
+import { RouteDiv, HOne } from "../../Resources/Components/RouteBox";
 import { useUserContext } from "../../Application/SelectLanguage/SelectLanguage";
 import axios from "axios";
 import {
   backDomain,
-  Xp,
-  UniversalButtonsDivFlex,
   DivFlex,
   DivMarginBorder,
 } from "../../Resources/UniversalComponents";
-import { alwaysWhite, secondaryColor } from "../../Styles/Styles";
-import { Button, CircularProgress } from "@mui/material";
-import { DivModal, InternDivModal } from "./Blog.Styled";
+import { secondaryColor } from "../../Styles/Styles";
+import { CircularProgress } from "@mui/material";
 import { MyHeadersType } from "../../Resources/types.universalInterfaces";
 import Helmets from "../../Resources/Helmets";
-import { notifyError } from "../EnglishLessons/Assets/Functions/FunctionLessons";
 import ReviewFlashCards from "../FlashCards/FlashCardsComponents/ReviewFlashCards";
 import BlogPosts from "./BlogPosts";
 
@@ -39,49 +30,9 @@ export function Blog({
   setChange,
 }: BlogProps) {
   const { UniversalTexts } = useUserContext();
-  // Strings
-  const [newTitle, setNewTitle] = useState<string>("");
-  const [_id, setID] = useState<string>("");
-  const [_StudentId, setStudentId] = useState<string>("");
-  const [newText, setNewText] = useState<string>("");
-  const [newImg, setNewImg] = useState<string>("");
-  const [newUrlVideo, setNewUrlVideo] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [classId, setClassId] = useState<string>("");
-  const [permissions, setPermissions] = useState<string>("");
-  // Booleans
-  const [seeConfirmDelete, setSeeConfirmDelete] = useState<boolean>(false);
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
-  // const [nextTutoring, setNextTutoring] = useState<any>();
-
-  // Loading
-  const [posts, setPosts] = useState<any>([
-    {
-      title: <CircularProgress style={{ color: secondaryColor() }} />,
-    },
-  ]);
-
   const [user, setUser] = useState<any>({});
-
-  // const fetchClasses = async (studentId: string) => {
-  //   setLoading(true);
-
-  //   try {
-  //     const response = await axios.get(
-  //       `${backDomain}/api/v1/homeworknext/${studentId}`,
-  //       {
-  //         headers: actualHeaders,
-  //       }
-  //     );
-  //     const tt = response.data.tutoringHomeworkList;
-  //     setNextTutoring(tt);
-  //     setLoading(false);
-  //   } catch (error) {
-  //     console.log(error, "erro ao listar itens");
-  //   }
-  // };
-
   var [course, setCourse] = useState<String>("");
   var [lesson, setLesson] = useState<String>("");
   var [img, setImg] = useState("");
@@ -121,24 +72,12 @@ export function Blog({
     } else {
     }
     let getLoggedUser = JSON.parse(localStorage.getItem("loggedIn") || "");
-    // fetchData();
     setClassId(getLoggedUser.lastClassId);
     setName(getLoggedUser.name);
-    setStudentId(getLoggedUser.id || _StudentId);
-    setPermissions(getLoggedUser.permissions);
-    // fetchClasses(getLoggedUser.id);
     setTimeout(() => {
       fetchLastClassId(getLoggedUser.lastClassId);
     }, 2000);
   }, []);
-
-  const handleSeeModal = () => {
-    setIsVisible(!isVisible);
-    setSeeConfirmDelete(false);
-  };
-  const handleConfirmDelete = () => {
-    setSeeConfirmDelete(!seeConfirmDelete);
-  };
 
   const actualHeaders = headers || {};
   const sessions = [
@@ -179,79 +118,10 @@ export function Blog({
     },
   ];
 
-  const editPost = async (id: string): Promise<void> => {
-    try {
-      const editedPost = {
-        title: newTitle,
-        videoUrl: newUrlVideo,
-        text: newText,
-        img: newImg,
-      };
-      const response = await axios.put(
-        `${backDomain}/api/v1/blogposts/${id}`,
-        editedPost,
-        { headers: actualHeaders }
-      );
-      fetchData();
-      handleSeeModal();
-    } catch (error) {
-      alert("Erro ao editar post");
-      console.error(error);
-      fetchData();
-      handleSeeModal();
-    }
-  };
-
-  const deletePost = async (id: string): Promise<void> => {
-    try {
-      const response = await axios.delete(
-        `${backDomain}/api/v1/blogposts/${id}`,
-        { headers: actualHeaders }
-      );
-      alert("Post definitivamente excluído");
-      handleSeeModal();
-      fetchData();
-    } catch (error) {
-      alert("Erro ao editar post");
-      console.error(error);
-      handleSeeModal();
-      fetchData();
-    }
-  };
-
-  async function fetchData(): Promise<void> {
-    setLoading(true);
-    try {
-      const response = await axios.get(`${backDomain}/api/v1/blogpost`, {
-        headers: actualHeaders,
-      });
-      setTimeout(() => {
-        const filteredPosts = response.data.listOfPosts.filter(
-          (post: any) => post !== null
-        );
-        setPosts(filteredPosts);
-        setLoading(false);
-      }, 300);
-    } catch (error: any) {
-      notifyError(error.response.data.error);
-      console.log(error.response.data.error);
-
-      setTimeout(() => {
-        if (error.response.data.feeUpToDate == false) {
-          window.location.assign("/feenotuptodate");
-        } else {
-          window.location.assign("/login");
-        }
-        setLoading(false);
-      }, 2500);
-    }
-  }
-
   return (
     <>
       <RouteDiv>
         <Helmets text="Home Page" />
-
         <div
           style={{
             margin: "1rem 0.5rem 0 0",
@@ -329,126 +199,6 @@ export function Blog({
           </div>
         </DivFlex>
       </RouteDiv>
-
-      <DivModal
-        className="modal"
-        style={{
-          display: isVisible ? "block" : "none",
-        }}
-      >
-        <Xp onClick={() => handleSeeModal()}>X</Xp>
-        <HTwo>{UniversalTexts.editPost}</HTwo>
-        <InternDivModal>
-          <input
-            value={newTitle}
-            onChange={(event) => setNewTitle(event.target.value)}
-            id="title"
-            placeholder="Title"
-            type="text"
-            className="inputs-style"
-          />
-          <input
-            className="inputs-style"
-            value={newUrlVideo}
-            onChange={(event) => setNewUrlVideo(event.target.value)}
-            id="VideoUrl"
-            placeholder="VideoUrl (Youtube/Vimeo)"
-            type="text"
-          />
-          <input
-            value={newImg}
-            onChange={(event) => setNewImg(event.target.value)}
-            id="VideoUrl"
-            placeholder="Imagem URL"
-            type="text"
-            className="inputs-style"
-          />
-          <textarea
-            value={newText}
-            onChange={(event) => setNewText(event.target.value)}
-            id="Texto"
-            placeholder="Texto"
-            cols={20}
-            rows={10}
-            required
-            className="inputs-style"
-          />
-        </InternDivModal>
-        <UniversalButtonsDivFlex
-          style={{
-            display: !seeConfirmDelete ? "flex" : "none",
-          }}
-        >
-          <Button
-            style={{
-              color: alwaysWhite(),
-              backgroundColor: "#ba3c3c",
-            }}
-            onClick={() => handleConfirmDelete()}
-          >
-            {UniversalTexts.delete}
-          </Button>
-          <Button
-            style={{
-              color: alwaysWhite(),
-              backgroundColor: "#194169",
-            }}
-            onClick={() => handleSeeModal()}
-          >
-            {UniversalTexts.cancel}
-          </Button>
-          <Button
-            style={{
-              color: alwaysWhite(),
-              backgroundColor: "#138017",
-            }}
-            onClick={() => editPost(_id)}
-          >
-            {UniversalTexts.save}
-          </Button>
-        </UniversalButtonsDivFlex>
-        <div
-          style={{
-            textAlign: "center",
-            margin: "0 auto",
-            display: seeConfirmDelete ? "grid" : "none",
-          }}
-        >
-          <p
-            style={{
-              color: "#ba3c3c",
-              paddingTop: "1rem",
-              fontWeight: 700,
-            }}
-          >
-            {UniversalTexts.deleteConfirm}
-          </p>
-          <UniversalButtonsDivFlex>
-            <Button
-              style={{
-                color: alwaysWhite(),
-                backgroundColor: "#194169",
-              }}
-              onClick={() => handleConfirmDelete()}
-            >
-              {UniversalTexts.no}
-            </Button>
-            <Button
-              style={{
-                color: alwaysWhite(),
-                backgroundColor: "#ba3c3c",
-              }}
-              onClick={() => deletePost(_id)}
-            >
-              {UniversalTexts.yes}
-            </Button>
-          </UniversalButtonsDivFlex>
-        </div>
-      </DivModal>
-      <BackgroundClickBlog
-        onClick={() => handleSeeModal()}
-        style={{ display: !isVisible ? "none" : "flex" }}
-      />
     </>
   );
 }
