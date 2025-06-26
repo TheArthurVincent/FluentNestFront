0ba1a1a91e7d87ab532136b0a490e57da1c59cc9
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-06-26 06:24:53 -0300

    Comment out Helmets component in BlogPosts for future reference.

--- src/Routes/HomePage/BlogPosts.tsx
+++ src/Routes/HomePage/BlogPosts.tsx
@@ -152,7 +152,7 @@ export function BlogPosts({ headers }: BlogPostsProps) {
   return (
     <>
       <div style={{ maxWidth: "900px" }}>
-        <Helmets text="Blog Posts" />
+        {/* <Helmets text="Blog Posts" /> */}
         <div
           style={{
             margin: "1rem 0.5rem 0 0",
