ba86121cefe94f05739a51e3f2152b6728397c76
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-11-15 06:39:08 -0300

    feat: Refactor RecommendedMaterials component to fetch lessons and improve rendering logic

--- src/Routes/ArvinComponents/GridHomePageComponents/RecommendedMaterials.tsx
+++ src/Routes/ArvinComponents/GridHomePageComponents/RecommendedMaterials.tsx
@@ -1,34 +1,7 @@
-import React, { FC, useEffect, useMemo } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { BooksIcon } from "@phosphor-icons/react";
 import { backDomain } from "../../../Resources/UniversalComponents";
 import axios from "axios";
-
-const tipsLesson = [
-  "Faça 10 frases na sessão 'Dictation' de sua última aula.",
-  "Se o dia estiver corrido, faça 5 frases na sessão 'Dictation' de sua última aula.",
-  "Faça 20 frases na sessão 'Dictation' de sua última aula.",
-  "Escolha uma aula de vocabulário para revisar.",
-  "Revise os pontos gramaticais abordados em sua última aula.",
-  "Assista a vídeos relacionados ao tema da sua última aula.",
-  "Pratique a pronúncia escutando os áudios das suas últimas aulas.",
-];
-
-const tipsFlashcards = [
-  "Revise seus flashcards para não esquecer o que aprendeu.",
-  "Tire o dia de hoje para revisar pelo menos 25 flashcards.",
-  "Tire o dia de hoje para revisar pelo menos 50 flashcards.",
-];
-
-const tipsHomework = [
-  "Já fez sua última Lição de Casa para a próxima aula?",
-  "Revisite suas últimas 3 Lições de Casa.",
-  "Anote dúvidas ao revisar suas Lições de Casa e pergunte ao professor.",
-];
-
-// ---- Função determinística com base no dia ----
-function pickByDay(array: string[], dayIndex: number) {
-  return array[dayIndex % array.length];
-}
 
 interface RecommendedMaterialsProps {
   appLoaded?: boolean;
@@ -43,14 +16,24 @@ export const RecommendedMaterials: FC<RecommendedMaterialsProps> = ({
   isDesktop,
   studentId,
 }) => {
+  const [lessons, setLessons] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // ====== GET recommended classes ======
   const seeLessons = async () => {
     try {
-      // const response = await axios.get(`${backDomain}/api/v1/score/${studentId}`, {
-      //   headers: actualHeaders ? { ...actualHeaders } : {},
-      // });
-      console.log("response.data");
+      const response = await axios.get(
+        `${backDomain}/api/v1/recommended-classes/${studentId}`,
+        {
+          headers: actualHeaders ? { ...actualHeaders } : {},
+        }
+      );
+
+      setLessons(response.data.lessons || []);
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -58,63 +41,55 @@ export const RecommendedMaterials: FC<RecommendedMaterialsProps> = ({
     seeLessons();
   }, [appLoaded, actualHeaders]);
 
-  const today = new Date().getDay();
-
-  const selectedTips = useMemo(() => {
-    return {
-      lesson: pickByDay(tipsLesson, today),
-      flashcards: pickByDay(tipsFlashcards, today),
-      homework: pickByDay(tipsHomework, today),
-    };
-  }, [today]);
-
   const styleLi = {
     fontFamily: "Plus Jakarta Sans",
     fontWeight: 600,
-    fontStyle: "SemiBold",
     fontSize: "13px",
     lineHeight: "22px",
-    letterSpacing: "0%",
     color: "#65748C",
-  };
+  } as React.CSSProperties;
 
   return (
-    <>
-      <span
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-          gap: "12px",
-        }}
-      >
+    !loading && (
+      <>
         <span
           style={{
             display: "flex",
-            gap: "8px",
+            flexDirection: "row",
             justifyContent: "space-between",
             alignItems: "center",
-            fontWeight: "600",
-            color: "#030303",
+            gap: "12px",
           }}
         >
-          <BooksIcon size={20} color={"#030303"} weight="bold" />
-          <span>Materiais Recomendados</span>
+          <span
+            style={{
+              display: "flex",
+              gap: "8px",
+              alignItems: "center",
+              fontWeight: "600",
+              color: "#030303",
+            }}
+          >
+            <BooksIcon size={20} color={"#030303"} weight="bold" />
+            <span>Materiais a Revisar</span>
+          </span>
         </span>
-      </span>
 
-      <div>
-        <ul
-          style={{
-            marginTop: "20px",
-          }}
-        >
-          <li style={styleLi}>{selectedTips.lesson}</li>
-          <li style={styleLi}>{selectedTips.homework}</li>
-          <li style={styleLi}>{selectedTips.flashcards}</li>
-        </ul>
-      </div>
-    </>
+        <div>
+          <ul style={{ marginTop: "20px" }}>
+            {lessons.map((item, index) => (
+              <li key={index} style={{ ...styleLi }}>
+                <a
+                  href={`/teaching-materials/${item.courseId}/${item._id}`}
+                  style={{ textDecoration: "none", color: "#65748C" }}
+                >
+                  {item.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </>
+    )
   );
 };
