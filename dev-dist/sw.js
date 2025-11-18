b065f40e1a1d0ea75b9d326b69656e5303f891ff
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-11-18 18:37:34 -0300

    feat: update service worker cache revision for index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7c5ad369'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.upprk2aqb7g"
+    "revision": "0.sdkcs63a3h8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
