37fd4e468a882a45f0ccab17674cefa797a205fc
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-11-25 14:31:01 -0300

    fix: Update video embed URL function in VideoLessonModel component

--- src/Routes/EnglishLessons/Assets/LessonsModels/VideoLessonModel.tsx
+++ src/Routes/EnglishLessons/Assets/LessonsModels/VideoLessonModel.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getVideoEmbedUrl } from "../../../../Resources/UniversalComponents";
 import { IFrameVideoBlog } from "../../../HomePage/Blog.Styled";
+import { getEmbedUrl } from "../../../MyCalendar/CalendarComponents/MyCalendarFunctions/MyCalendarFunctions";
 interface VideoLessonModelProps {
   element: any;
 }
@@ -14,7 +15,7 @@ export default function VideoLessonModel({ element }: VideoLessonModelProps) {
         marginTop: "1rem",
       }}
     >
-      <IFrameVideoBlog src={getVideoEmbedUrl(element.video)} />
+      <IFrameVideoBlog src={getEmbedUrl(element.video)} />
     </div>
   );
 }
