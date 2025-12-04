6f7b951a4365cb2c4978b4e89f0ae8365855dca9
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-12-04 20:18:06 -0300

    feat: add InstallPWA component for prompting users to install the app feat: integrate Groups component into Turmas for enhanced group management feat: pass _StudentId to ArvinNewHomePage for improved data handling

--- src/Routes/ArvinNewHomePage.tsx
+++ src/Routes/ArvinNewHomePage.tsx
@@ -452,6 +452,7 @@ export function ArvinNewHomePage({ headers }: HeadersProps) {
           change={change}
           setChange={setChange}
           isDesktop={isDesktop}
+          id={_StudentId}
         />
       ),
     },
