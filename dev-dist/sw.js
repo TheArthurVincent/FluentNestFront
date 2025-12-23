eab5b04135cdf9c4cf832527a03f85bb586c2048
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-12-23 19:17:09 -0300

    fix: update revision number for index.html in service worker and source map

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7c5ad369'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tjeacda7f38"
+    "revision": "0.741nstmep7g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
