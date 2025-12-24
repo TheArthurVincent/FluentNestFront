b6f21be57b07929fb5e694c2238be0db74c099de
TheArthurVincent <arthurcardosocorp@gmail.com>
2025-12-24 12:58:04 -0300

    feat: add NewClassModal component for creating new classes with form validation and modal functionality

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7c5ad369'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.741nstmep7g"
+    "revision": "0.489h7qsfqn"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
