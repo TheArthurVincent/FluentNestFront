ad68d2273b0d2c1d76958388bac02c58cf7c0332
TheArthurVincent <arthurcardosocorp@gmail.com>
2026-01-30 20:03:34 -0300

    fix: update service worker revision hashes and reorder menu items in sidebar

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7c5ad369'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ivvknoo6v2"
+    "revision": "0.up8034i3g4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
