1d2fcc6b26cc44e3f6022d636a3f216a68201814
TheArthurVincent <arthurcardosocorp@gmail.com>
2026-03-02 18:10:26 -0300

    fix: update date formatting in StudentTodayClassesCard component

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-7c5ad369'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g58l7rbceu"
+    "revision": "0.9n5vl5cbl1g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
